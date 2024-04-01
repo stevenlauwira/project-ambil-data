65b75cdebfc5e817bd3f0f9ab97b59ac20bf7a5b
stev en <stevenlw12@gmail.com>
2024-04-01 15:10:49 +0700

    fix some bug

--- bmkg.py
+++ bmkg.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 # Fungsi untuk login ke situs
 def login(Email, Kata_Sandi):
-    login_url = 'https://dataonline.bmkg.gosteven.id/home'
+    login_url = 'https://dataonline.bmkg.go.id/home'
     session = requests.Session()
     login_data = {
         'Email': Email,
@@ -24,8 +24,6 @@ def get_data(session):
     base_url = 'https://dataonline.bmkg.go.id/home'
     response = session.get(base_url)
     if response.status_code == 200:
-        # Lakukan sesuatu dengan data yang diperoleh
-        # Misalnya, olah data atau simpan ke dalam file
         print('Data berhasil diperoleh!')
     else:
         print('Gagal mendapatkan data.')
